9470c7bf537c10ced2c0277402fc49580cb316e2
Florian Danhiez <florian.danhiez@apprenant.ifapme.be>
2022-12-12 19:40:07 +0100

    ex02

---
+++ ex02/index.js
@@ -0,0 +1,48 @@
+class Vehicule {
+  // Constructeur
+  constructor(marque, modele, vitesseMax) {
+    this.marque = marque;
+    this.modele = modele;
+    this.vitesseMax = vitesseMax;
+  }
+
+  // Méthode pour afficher les détails du véhicule
+  afficherDetails() {
+    console.log(`Marque : ${this.marque}`);
+    console.log(`Modèle : ${this.modele}`);
+    console.log(`Vitesse maximale : ${this.vitesseMax}`);
+
+  }
+
+  // Méthode pour mettre à jour la vitesse maximale
+  mettreAJourVitesseMax(nouvelleVitesseMax) {
+    this.vitesseMax = nouvelleVitesseMax;
+  }
+}
+
+class Moto extends Vehicule {
+  // Constructeur
+  constructor(marque, modele, vitesseMax,nombreDeRoues) {
+    // Appeler le constructeur parent
+    super(marque, modele, vitesseMax, 2);
+    
+    // Initialiser la propriété spécifique à la moto
+    this.nombreDeRoues = nombreDeRoues;
+  }
+
+  // Méthode pour afficher les détails de la moto
+  afficherDetailsMoto() {
+   
+    // Appeler la méthode afficherDetails() du parent
+    afficherDetails(Vehicule);
+    // Afficher la propriété spécifique à la moto
+    console.log(`Nombre de roues : ${this.nombreDeRoues}`);
+  }
+  
+}
+
+
+const maMoto = new Moto("Harley-Davidson", "Iron 883", 180, 2);
+maMoto.afficherDetails();
+maMoto.mettreAJourVitesseMax(200);
+maMoto.afficherDetails();
